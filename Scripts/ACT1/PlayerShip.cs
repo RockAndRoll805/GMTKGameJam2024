96c2e82ff2348ed75d7602c42f9b5366c6cb63cd
Sasank Madineni <sasank1020@gmail.com>
2024-08-16 18:56:48 -0700

    basic ship controller

---
+++ Scripts/ACT1/PlayerShip.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class PlayerShip : CharacterBody2D
+{
+	[Export] private int movespeed = 250;
+
+	private AnimatedSprite2D sprite;
+	private CollisionShape2D collider;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		sprite = (AnimatedSprite2D) GetNode("AnimatedSprite2D");
+		collider = (CollisionShape2D) GetNode("CollisionShape2D");	
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		HandleMove();
+
+		MoveAndSlide();
+	}
+
+	private void HandleMove() {
+		
+		// Godot.Vector2 velocity = Velocity;
+		Godot.Vector2 input_dir = new(Input.GetAxis("ui_left", "ui_right"), Input.GetAxis("ui_up", "ui_down"));
+		GD.Print(input_dir);
+
+		Godot.Vector2 velocity = input_dir * movespeed;
+		if(input_dir.X != 0) {
+			sprite.FlipH = input_dir.X < 0;
+		}
+
+		if(input_dir.Y != 0){
+			sprite.FlipV = input_dir.Y < 0;
+		}
+
+		Velocity = velocity;
+
+	}
+}
---
+++ Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class CameraController : Camera2D
+{
+
+	[Export] private Node2D target;
+	[Export] private int tolerance_dist = 5;
+	[Export] private Vector2 zoom = new(1, 1);
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		this.Zoom = new(1, 1);
+		this.Position = target.Position;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		// smooth camera
+		Vector2 dist_to_target = target.Position - this.Position;
+		if(dist_to_target.Length() > tolerance_dist) {
+			this.Position += dist_to_target.Normalized() * dist_to_target.Length()*0.1f;
+		}
+	}
+}
